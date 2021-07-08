7f119de7845114ebce7a1bfd7c564b90baf2241c
antares <antares@wormhole.studio>
2021-07-08 19:57:34 +0800

    add suport method

--- src/main/java/org/starcoin/utils/BcsSerializeHelper.java
+++ src/main/java/org/starcoin/utils/BcsSerializeHelper.java
@@ -40,16 +40,6 @@ public class BcsSerializeHelper {
   }
 
 
-//  @SneakyThrows
-//  public static Bytes serializeVectorU8(List<Byte> value) {
-//    BcsSerializer serializer = new com.novi.bcs.BcsSerializer();
-//    serializer.serialize_len(value.size());
-//    for (@com.novi.serde.Unsigned Byte item : value) {
-//      serializer.serialize_u8(item);
-//    }
-//    byte[] b = serializer.get_bytes();
-//    return Bytes.valueOf(b);
-//  }
 
 
   @SneakyThrows
@@ -64,4 +54,55 @@ public class BcsSerializeHelper {
     byte[] bytes = s.get_bytes();
     return Bytes.valueOf(bytes);
   }
+
+
+
+  @SneakyThrows
+  public static byte[] serializeU64(Long v) {
+    long value = v.longValue();
+    com.novi.serde.Serializer serializer = new com.novi.bcs.BcsSerializer();
+    serializer.increase_container_depth();
+    serializer.serialize_u64(value);
+    serializer.decrease_container_depth();
+    return serializer.get_bytes();
+  }
+
+  @SneakyThrows
+  public static byte[] serializeU128(BigInteger v) {
+    com.novi.serde.Serializer serializer = new com.novi.bcs.BcsSerializer();
+    serializer.increase_container_depth();
+    serializer.serialize_u128(v);
+    serializer.decrease_container_depth();
+    return serializer.get_bytes();
+  }
+
+  @SneakyThrows
+  public static byte[] serializeString(String v) {
+    com.novi.serde.Serializer serializer = new com.novi.bcs.BcsSerializer();
+    serializer.increase_container_depth();
+    serializer.serialize_str(v);
+    serializer.decrease_container_depth();
+    return serializer.get_bytes();
+  }
+
+  @SneakyThrows
+  public static byte[] serializeU8(Byte v) {
+    com.novi.serde.Serializer serializer = new com.novi.bcs.BcsSerializer();
+    serializer.increase_container_depth();
+    serializer.serialize_u8(v);
+    serializer.decrease_container_depth();
+    return serializer.get_bytes();
+  }
+
+  @SneakyThrows
+  public static byte[] serializeVectorU8(List<Byte> value) {
+    com.novi.serde.Serializer serializer = new com.novi.bcs.BcsSerializer();
+
+    serializer.serialize_len(value.size());
+    for (@com.novi.serde.Unsigned Byte item : value) {
+      serializer.serialize_u8(item);
+    }
+    return serializer.get_bytes();
+  }
+
 }

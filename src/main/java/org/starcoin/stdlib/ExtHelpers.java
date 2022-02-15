6c74c081536073504253107b5b24ff1efa0b7d2f
yangjiefeng <yangjiefeng@gmail.com>
2022-02-15 22:33:10 +0800

    and some handy TransactionPayload encoding methods

--- src/main/java/org/starcoin/stdlib/ExtHelpers.java
+++ src/main/java/org/starcoin/stdlib/ExtHelpers.java
@@ -5,11 +5,13 @@ import com.novi.serde.Bytes;
 import com.novi.serde.Int128;
 import com.novi.serde.SerializationError;
 import com.novi.serde.Unsigned;
+import org.jetbrains.annotations.NotNull;
 import org.starcoin.types.*;
 import org.starcoin.utils.AccountAddressUtils;
 
 import java.math.BigInteger;
 import java.util.Collections;
+import java.util.List;
 
 public class ExtHelpers {
 
@@ -17,38 +19,71 @@ public class ExtHelpers {
     }
 
     public static TransactionPayload encode_empty_args_script_function(String moduleId, String functionName) {
-        ScriptFunction.Builder script_function_builder = new ScriptFunction.Builder();
-        script_function_builder.ty_args = java.util.Arrays.asList();
-        script_function_builder.args = Collections.emptyList();
-        script_function_builder.function = new Identifier(functionName);
-        script_function_builder.module = parseModuleId(moduleId);
-        TransactionPayload.ScriptFunction.Builder builder = new TransactionPayload.ScriptFunction.Builder();
-        builder.value = script_function_builder.build();
-        return builder.build();
+        return buildScriptFunctionTransactionPayload(moduleId, functionName, Collections.emptyList());
+    }
+
+    public static TransactionPayload encode_u8_script_function(String moduleId, String functionName, Byte b) {
+        return buildScriptFunctionTransactionPayload(moduleId, functionName, Collections.singletonList(
+                encode_u8_argument(b)
+        ));
+    }
+
+    public static TransactionPayload encode_u64_script_function(String moduleId, String functionName, Long l) {
+        return buildScriptFunctionTransactionPayload(moduleId, functionName, Collections.singletonList(
+                encode_u64_argument(l)
+        ));
+    }
+
+    public static TransactionPayload encode_u128_script_function(String moduleId, String functionName, BigInteger i) {
+        return buildScriptFunctionTransactionPayload(moduleId, functionName, Collections.singletonList(
+                encode_u128_argument(i)
+        ));
+    }
+
+    public static TransactionPayload encode_bool_script_function(String moduleId, String functionName, Boolean b) {
+        return buildScriptFunctionTransactionPayload(moduleId, functionName, Collections.singletonList(
+                encode_bool_argument(b)
+        ));
     }
 
     public static TransactionPayload encode_u64_and_u8_script_function(String moduleId, String functionName, Long u64, Byte u8) {
-        ScriptFunction.Builder script_function_builder = new ScriptFunction.Builder();
-        script_function_builder.ty_args = java.util.Arrays.asList();
-        script_function_builder.args = java.util.Arrays.asList(
+        return buildScriptFunctionTransactionPayload(moduleId, functionName, java.util.Arrays.asList(
                 encode_u64_argument(u64),
                 encode_u8_argument(u8)
+        ));
+    }
+
+    public static TransactionPayload encode_u8vector_script_function(String moduleId, String functionName, Bytes bytes) {
+        java.util.List<com.novi.serde.Bytes> args = java.util.Arrays.asList(
+                encode_u8vector_argument(bytes)
         );
-        script_function_builder.function = new Identifier(functionName);
-        script_function_builder.module = parseModuleId(moduleId);
-        TransactionPayload.ScriptFunction.Builder builder = new TransactionPayload.ScriptFunction.Builder();
-        builder.value = script_function_builder.build();
-        return builder.build();
+        return buildScriptFunctionTransactionPayload(moduleId, functionName, args);
     }
 
     public static TransactionPayload encode_u8vector_and_u64_and_bool_script_function(String moduleId, String functionName, Bytes bytes, Long u64, Boolean b) {
-        ScriptFunction.Builder script_function_builder = new ScriptFunction.Builder();
-        script_function_builder.ty_args = java.util.Arrays.asList();
-        script_function_builder.args = java.util.Arrays.asList(
+        java.util.List<com.novi.serde.Bytes> args = java.util.Arrays.asList(
                 encode_u8vector_argument(bytes),
                 encode_u64_argument(u64),
                 encode_bool_argument(b)
         );
+        return buildScriptFunctionTransactionPayload(moduleId, functionName, args);
+    }
+
+    public static TransactionPayload encode_u8vector_and_u64_script_function(String moduleId, String functionName, Bytes bytes, Long u64) {
+        return buildScriptFunctionTransactionPayload(
+                moduleId,
+                functionName,
+                java.util.Arrays.asList(
+                        encode_u8vector_argument(bytes),
+                        encode_u64_argument(u64)
+                ));
+    }
+
+    @NotNull
+    private static TransactionPayload buildScriptFunctionTransactionPayload(String moduleId, String functionName, List<Bytes> args) {
+        ScriptFunction.Builder script_function_builder = new ScriptFunction.Builder();
+        script_function_builder.ty_args = Collections.emptyList();
+        script_function_builder.args = args;
         script_function_builder.function = new Identifier(functionName);
         script_function_builder.module = parseModuleId(moduleId);
         TransactionPayload.ScriptFunction.Builder builder = new TransactionPayload.ScriptFunction.Builder();
@@ -56,19 +91,6 @@ public class ExtHelpers {
         return builder.build();
     }
 
-    public static TransactionPayload encode_u8vector_and_u64_script_function(String moduleId, String functionName, Bytes bytes, Long u64) {
-        ScriptFunction.Builder script_function_builder = new ScriptFunction.Builder();
-        script_function_builder.ty_args = java.util.Arrays.asList();
-        script_function_builder.args = java.util.Arrays.asList(
-                encode_u8vector_argument(bytes),
-                encode_u64_argument(u64)
-        );
-        script_function_builder.function = new Identifier(functionName);
-        script_function_builder.module = parseModuleId(moduleId);
-        TransactionPayload.ScriptFunction.Builder builder = new TransactionPayload.ScriptFunction.Builder();
-        builder.value = script_function_builder.build();
-        return builder.build();
-    }
 
     private static ModuleId parseModuleId(String m) {
         String[] ss = m.split("::");

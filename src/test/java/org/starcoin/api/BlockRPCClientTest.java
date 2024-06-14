f983fb8b0eb1fe86ebf0291b10ff953e64b62a95
bobong <libo.weng@gmail.com>
2024-06-14 18:17:07 +0800

    [flexi dag inspector] add rpc call `chain.get_ghostdagdata`

--- src/test/java/org/starcoin/api/BlockRPCClientTest.java
+++ src/test/java/org/starcoin/api/BlockRPCClientTest.java
@@ -3,6 +3,7 @@ package org.starcoin.api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import junit.framework.TestCase;
 import org.starcoin.bean.Block;
+import org.starcoin.bean.BlockGhostdagData;
 import org.starcoin.bean.BlockHeader;
 import org.starcoin.bean.BlockInfo;
 import org.starcoin.jsonrpc.client.JSONRPC2SessionException;
@@ -16,7 +17,8 @@ public class BlockRPCClientTest extends TestCase {
     @Override
     protected void setUp() throws Exception {
         //client = new ContractRPCClient(new URL("http://localhost:9850"));
-        client = new BlockRPCClient(new URL("https://barnard-seed.starcoin.org"));
+        client = new BlockRPCClient(new URL("https://halley-seed.starcoin.org"));
+        // client = new BlockRPCClient(new URL("http://127.0.0.1:9850"));
     }
 
     public void testGetChainHeader() throws JsonProcessingException {
@@ -57,8 +59,20 @@ public class BlockRPCClientTest extends TestCase {
 
     public void testGetBlockListFromHeight() {
         try {
-            List<Block> blockList = client.getBlockListFromHeight(34401, 10);
+            List<Block> blockList = client.getBlockListFromHeight(/*34401*/ 100, 10);
             System.out.println(blockList.size());
+        } catch (JSONRPC2SessionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testGetBlockGhostdagData() {
+        try {
+            BlockGhostdagData ghostdagData = client.getBlockGhostdagData("0x5b63caad86f3240e73e4d6f5b2cb58025ff2e86eecc96da10ad551fd1a6f70e6");
+            // assertNotNull(ghostdagData);
+            if (ghostdagData != null) {
+                System.out.println(ghostdagData);
+            }
         } catch (JSONRPC2SessionException e) {
             e.printStackTrace();
         }
